3031947ed3a4443172973e86c585ad9f3318b67a
jyajoo <jyajoo1020@gmail.com>
2021-07-25 12:00:00 +0900

    Initial Commit

---
+++ src/test/java/org/inu/universe/UniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inu.universe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
